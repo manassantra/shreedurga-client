38ecc9c301ec96889feabeac82cfdfb95fe9a932
MANAS SANTRA <47280250+manassantra@users.noreply.github.com>
2024-10-29 00:20:54 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URI: 'http://localhost:8089/api/',
+    API_URI: 'https://shreedurga-server.onrender.com/api/',
     API_KEY: 'Kp6yj9kSOob7BLHJdIVth1WhEIYHUmyeWgduJgPTIK616naEWGoDwzFY6BFeR5u9',
     CLIENT_SCRET: 'xDZT5l6bE4RbD4A8rBrMNE49zBN3Dzgu'
 };
